d14f304b03c30dbfe2ebfe18d1adf3c3603deb9f
Abhi1997 <41516895+Abhi1997@users.noreply.github.com>
2025-01-23 09:46:33 +0545

    feat:added percentage feature

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,4 +2,9 @@ public class GradeCalculator {
     public float getGrade(float math, float physics) {
         return math + physics;
     }
+    
+    public float getPercentage(float math, float physics) {
+        return (math + physics/200)*100;
+    }
+
 }
